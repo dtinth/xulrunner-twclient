bc5393932316ec32eeff81bfd8bab80bb6fc46af
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2012-07-18 11:42:02 +0700

    add full-screen, zooming

--- tw/chrome/content/main.js
+++ tw/chrome/content/main.js
@@ -10,6 +10,26 @@ window.addEventListener('load', function() {
   browser.addEventListener("DOMTitleChanged", titleChanged, true)
   browser.addEventListener("load", load, true)
 
+  ;(function() {
+    var zoom = [0.2, 0.4, 0.6, 0.8, 1, 1.25, 1.5, 1.75, 2, 2.5, 3]
+      , current = 4
+    function zoomIn() {
+      current += 1
+      if (current > zoom.length - 1) current = zoom.length
+      browser.markupDocumentViewer.fullZoom = zoom[current]
+    }
+    function zoomOut() {
+      current -= 1
+      if (current < 0) current = 0
+      browser.markupDocumentViewer.fullZoom = zoom[current]
+    }
+    function zoomReset() {
+      current = 4
+      browser.markupDocumentViewer.fullZoom = zoom[current]
+    }
+    window.zoom = { zoomIn: zoomIn, zoomOut: zoomOut, reset: zoomReset }
+  })()
+
   function titleChanged(e) {
     if (e.target != browser.contentDocument) return
     document.title = browser.contentDocument.title
