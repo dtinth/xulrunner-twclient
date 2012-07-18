b879c650a5f849275ba0d778854864698baa0a98
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2012-07-18 10:24:56 +0700

    .zip -> .app.zip

--- osx/archive.rb
+++ osx/archive.rb
@@ -6,4 +6,4 @@ ENV['CONFIGURATION'] = 'Release'
 
 system('./build.sh') or fail 'cannot create thaiWitter.app'
 Dir.exists?('thaiWitter.app') or fail 'thaiWitter.app does not exists'
-system('ditto -c -k --sequesterRsrc --keepParent thaiWitter.app ' + TwClient::build_name + '.zip')
+system('ditto -c -k --sequesterRsrc --keepParent thaiWitter.app ' + TwClient::build_name + '.app.zip')
